f69e39e4c5139d189d175d30920315cbe0dd7426
avelitj12 <avelitj12@gmail.com>
2025-03-23 08:58:46 -0400

    Support checking if stopped spinners line up

--- SCRIPTS/playground.gd
+++ SCRIPTS/playground.gd
@@ -3,6 +3,9 @@ extends Node
 @onready var spinner_1: Spinner = $MiddleRow/Spinners/Spinner
 @onready var spinner_2: Spinner = $MiddleRow/Spinners/Spinner2
 @onready var spinner_3: Spinner = $MiddleRow/Spinners/Spinner3
+@onready var score: Label = $TopRow/ScoreBox/ScoreRow/Score
+
+var stopped_spinners : Array[Spinner]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,3 +23,18 @@ func _on_stop_two_pressed() -> void:
 
 func _on_stop_three_pressed() -> void:
 	spinner_3.stop()
+
+func _on_spinner_stopped(stopped_spinner: Spinner) -> void:
+	if not stopped_spinner in stopped_spinners:
+		stopped_spinners.append(stopped_spinner)
+	if len(stopped_spinners) == 3:
+		print(_check_spinner_lineup())
+
+func _check_spinner_lineup():
+	var prev_position_y = null
+	for spinner in stopped_spinners:
+		if not prev_position_y:
+			prev_position_y = int(spinner.get_icons_position().y)
+		elif prev_position_y != int(spinner.get_icons_position().y):
+			return false
+	return true
