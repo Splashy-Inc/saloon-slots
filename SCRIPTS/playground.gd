6ee50319b5df4ea579e42d71c44c6dede4ddca4d
avelitj12 <avelitj12@gmail.com>
2025-03-23 14:25:34 -0400

    Add limited spins

--- SCRIPTS/playground.gd
+++ SCRIPTS/playground.gd
@@ -3,11 +3,14 @@ extends Node
 @onready var spinner_1: Spinner = $MiddleRow/Spinners/Spinner
 @onready var spinner_2: Spinner = $MiddleRow/Spinners/Spinner2
 @onready var spinner_3: Spinner = $MiddleRow/Spinners/Spinner3
-@onready var score_label: Label = $TopRow/ScoreBox/ScoreRow/ScoreLabel
+@onready var score_label: Label = $TopRow/InfoSection/InfoBox/ScoreBox/ScoreLabel
+@onready var spins_count: Label = $TopRow/InfoSection/InfoBox/SpinsBox/SpinsCount
 @onready var lever: Lever = $MiddleRow/Lever/Lever
 
-const SCORE_INCREMENT = 100
-var score = 0
+const SCORE_INCREMENT := 100
+var score := 0
+const STARTING_SPINS = 10
+var spins_left := STARTING_SPINS
 var stopped_spinners : Array[Spinner]
 
 # Called when the node enters the scene tree for the first time.
@@ -34,7 +37,8 @@ func _on_spinner_stopped(stopped_spinner: Spinner) -> void:
 		if _check_spinner_lineup():
 			score += SCORE_INCREMENT
 			score_label.text = str(score)
-		lever.reset()
+		if spins_left > 0:
+			lever.reset()
 
 
 func _check_spinner_lineup():
@@ -45,8 +49,17 @@ func _check_spinner_lineup():
 		elif prev_position_y != int(spinner.get_icons_position().y):
 			return false
 	return true
+
 func _on_lever_pulled() -> void:
-	stopped_spinners = []
-	spinner_1.start()
-	spinner_2.start()
-	spinner_3.start()
+	if _update_spins_left(spins_left - 1):
+		stopped_spinners = []
+		spinner_1.start()
+		spinner_2.start()
+		spinner_3.start()
+
+func _update_spins_left(spins: int):
+	if spins_left > 0:
+		spins_left = clamp(spins, 0, STARTING_SPINS)
+		spins_count.text = str(spins_left)
+		return true
+	return false
